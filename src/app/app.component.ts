3c60eaf82e9b43b1ea650fac8eb616a7d34c734a
unknown <nitin.kumar@primussoft.com-at-418809518040>
2019-12-18 20:27:47 +0530

    creating service in angular

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,10 +1,20 @@
-import { Component } from '@angular/core';
+import { Component, OnInit } from '@angular/core';
+import { MyserviceService } from './myservice.service';
 
 @Component({
   selector: 'app-root',
   templateUrl: './app.component.html',
   styleUrls: ['./app.component.css']
 })
-export class AppComponent {
+export class AppComponent  implements OnInit {
   title = 'AngularServices';
+  todaydate;
+
+  constructor(private myService: MyserviceService) {
+
+  }
+
+  ngOnInit() {
+    this.todaydate = this.myService.todayDate();
+  }
 }
